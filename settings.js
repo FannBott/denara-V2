4dd5eda2130052facae4193f373f85e06786a8ce
FannBott <108573537+FannBott@users.noreply.github.com>
2022-11-16 21:58:38 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "SATRIA - BOT" //namabot kalian
-global.ownername= "SATRIA -MD" //nama kalian
+global.botname = "satria" //namabot kalian
+global.ownername= "satria ganteng" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/FANNZ ID" //bebas asal jan hapus
 global.myweb2 = "https://linki.ee/denarabot" //bebas
 global.email = "satria8xc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['8562056436556'] //premium user
-global.owner = ["8562056436556","66827294199"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "8562056436556" // nomor wa kalian
-global.ownernomerr = "+8562056436556" //nmr wa kalian
+global.premium = ['855388410032'] //premium user
+global.owner = ["855388410032","855388410032"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "855388410032" // nomor wa kalian
+global.ownernomerr = "+855388410032" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© PUNYA SATRIA INI' //sticker wm ubah
-global.author = 'Di Buat Oleh SATRIA - BOT' //sticker wm ganti nama kalian
+global.packname = '© punya sahaa?' //sticker wm ubah
+global.author = 'Di Buat Oleh satria' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
